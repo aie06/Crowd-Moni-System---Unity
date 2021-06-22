8769e933e59dd83688a1822561c6139f3e1a9d61
aie06 <76236769+aie06@users.noreply.github.com>
2021-06-23 00:40:52 +0800

    asd

--- Sql/Assets/ViewInformation.cs
+++ Sql/Assets/ViewInformation.cs
@@ -2,31 +2,135 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Data.SqlClient;
+using System.Data;
+using System;
+
 using TMPro;
 
 public class ViewInformation : MonoBehaviour
 {
-    public Transform entryContainer;
-    public Transform entryTemplate;
-    public TMP_Text id;
-    public TMP_Text courseAndYr;
-    public TMP_Text lastName;
-    public TMP_Text fistName;
-    public TMP_Text middleName;
+    public enum BuildingName {None,B1,B2,B3}
+    public enum FloorLevel {None,Ground,Second,Third}
+    public enum RoomNumber {None,Room1,Room2,Room3,Room4}
+
+    [Header("Table Viewer Settings")]
+    public Transform viewerContainer;
+    public GameObject dataEntry;
+
+    [Header("Attendance Viewer Settings")]
+    public Transform attendanceContainer;
+    public BuildingName Building = BuildingName.None;
+    public FloorLevel Floor = FloorLevel.None;
+    public RoomNumber Room = RoomNumber.None;
+
+    string query;
+    string connectionString = @"Data Source=DESKTOP-SSEOURC\SQLEXPRESS,1433;Initial Catalog = CROWD_MONITORING_SYSTEM; User ID = sa; Password=adminaie";
+    SqlConnection con;
+    SqlCommand cmd;
+    SqlDataReader rd;
+
+    #region Data Viewer
     public void ViewTableData()
     {
-        
-        entryTemplate.gameObject.SetActive(false);
+        ResetViewData();
+        con = new SqlConnection(connectionString);
+        //entryTemplate.gameObject.SetActive(false);
 
-        float templateView = 20f;
-        for (int i = 0; i < 20; i++)
+        con.Open();
+        if (con.State == ConnectionState.Open)
         {
-            Transform entryTransform = Instantiate(entryTemplate, entryContainer);
-            RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
-            entryRectTransform.anchoredPosition = new Vector2(0, -templateView * i);
-            entryTransform.gameObject.SetActive(true);
+
+            query = "SELECT * FROM INFORMATION";
+            cmd = new SqlCommand(query, con);
+            rd = cmd.ExecuteReader();
+
+            float templateView = 20f;
+            while (rd.Read()) {
+                GameObject obj = Instantiate(dataEntry, viewerContainer);
+                obj.GetComponent<DataEntry>().Initialize(
+                    rd["ID"].ToString(),
+                    rd["COURSE_AND_YEAR"].ToString(),
+                    rd["LAST_NAME"].ToString(),
+                    rd["FIRST_NAME"].ToString(),
+                    rd["MIDDLE_NAME"].ToString());
+                obj.transform.localScale = Vector3.one;
+
+                //entryTransform = Instantiate(entryTemplate, entryContainer);
+                //RectTransform entryRectTransform = entryTransform.GetComponent<RectTransform>();
+                //entryRectTransform.anchoredPosition = new Vector2(0, -templateView * update);
+                //entryTransform.gameObject.SetActive(true);
+                //update++;
+            }
         }
-
-
+  
     }
+    public void ResetViewData()
+    {
+        if(viewerContainer.childCount > 0)
+        {
+            GameObject[] entries = new GameObject[viewerContainer.childCount];
+            for (int i = 0; i < entries.Length; i++)
+                entries[i] = viewerContainer.GetChild(i).gameObject;
+            
+            for (int i = 0; i < entries.Length; i++)
+            {
+                Destroy(entries[i]);
+            }
+        }
+    }
+    #endregion
+
+    #region Attendance Viewer
+    public void ViewAttendance()
+    {
+        ResetAttendanceData();
+        con = new SqlConnection(connectionString);
+        //entryTemplate.gameObject.SetActive(false);
+
+        con.Open();
+        if (con.State == ConnectionState.Open)
+        {
+
+            query = "SELECT * FROM INFORMATION "+((Building!=BuildingName.None)?"WHERE "+((Building==BuildingName.None)? "" : "ID=15151515"):"");
+            Debug.Log(query);
+            cmd = new SqlCommand(query, con);
+            rd = cmd.ExecuteReader();
+
+            float templateView = 20f;
+            while (rd.Read())
+            {
+                GameObject obj = Instantiate(dataEntry, attendanceContainer);
+                obj.GetComponent<DataEntry>().Initialize(
+                    rd["ID"].ToString(),
+                    rd["COURSE_AND_YEAR"].ToString(),
+                    rd["LAST_NAME"].ToString(),
+                    rd["FIRST_NAME"].ToString(),
+                    rd["MIDDLE_NAME"].ToString());
+                obj.transform.localScale = Vector3.one;
+            }
+        }
+    }
+    public void SetBuilding(TMP_Dropdown building)
+    { Building = EnumHelper.GetEnum<BuildingName>(building.value); }
+    public void SetFloor(TMP_Dropdown floor)
+    { Floor = EnumHelper.GetEnum<FloorLevel>(floor.value); }
+    public void SetRoom(TMP_Dropdown room)
+    { Room = EnumHelper.GetEnum<RoomNumber>(room.value); }
+    public void ResetAttendanceData()
+    {
+        if (attendanceContainer.childCount > 0)
+        {
+            GameObject[] entries = new GameObject[attendanceContainer.childCount];
+            for (int i = 0; i < entries.Length; i++)
+                entries[i] = attendanceContainer.GetChild(i).gameObject;
+
+            for (int i = 0; i < entries.Length; i++)
+            {
+                Destroy(entries[i]);
+            }
+        }
+    }
+
+    #endregion
 }
