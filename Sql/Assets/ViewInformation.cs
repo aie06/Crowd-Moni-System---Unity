c5ebea85ed5e5b76a3e36950439d1cf6f2b3a27b
ch@rold <gonchin088@gmail.com>
2021-06-24 05:12:48 +0800

    fsdsdfs

--- Sql/Assets/ViewInformation.cs
+++ Sql/Assets/ViewInformation.cs
@@ -22,6 +22,12 @@ public class ViewInformation : MonoBehaviour
     [Header("Attendance Viewer Settings")]
     public Transform attendanceContainer;
     public GameObject dataEntryAttendance;
+    public TMP_Dropdown filterBuilding;
+    public TMP_Text buildingEmpty;
+    public TMP_Dropdown filterFloor;
+    public TMP_Text floorEmpty;
+    public TMP_Dropdown filterRoom;
+    public TMP_Text roomEmpty;
     public BuildingName Building = BuildingName.None;
     public FloorLevel Floor = FloorLevel.None;
     public RoomNumber Room = RoomNumber.None;
@@ -101,7 +107,7 @@ public class ViewInformation : MonoBehaviour
         {
 
             //query = "SELECT * FROM INFORMATION "+((Building!=BuildingName.None)?"WHERE "+((Building==BuildingName.None)? "" : "ID=15151515"):"");
-            query = "SELECT * FROM ATTENDANCE ";
+            query = "SELECT NAME,COURSE_AND_YEAR,TIME,REMARKS FROM ATTENDANCE";
             //Debug.Log(query);
             cmd = new SqlCommand(query, con);
             rd = cmd.ExecuteReader();
@@ -110,7 +116,7 @@ public class ViewInformation : MonoBehaviour
             {
                 GameObject obj = Instantiate(dataEntryAttendance, attendanceContainer);
                 obj.GetComponent<DataEntryAttendance>().Initialize(
-                    rd["nAME"].ToString(),
+                    rd["NAME"].ToString(),
                     rd["COURSE_AND_YEAR"].ToString(),
                     rd["TIME"].ToString(),
                     rd["REMARKS"].ToString());
@@ -118,12 +124,12 @@ public class ViewInformation : MonoBehaviour
             }
         }
     }
-    public void SetBuilding(TMP_Dropdown building)
-    { Building = EnumHelper.GetEnum<BuildingName>(building.value); }
-    public void SetFloor(TMP_Dropdown floor)
-    { Floor = EnumHelper.GetEnum<FloorLevel>(floor.value); }
-    public void SetRoom(TMP_Dropdown room)
-    { Room = EnumHelper.GetEnum<RoomNumber>(room.value); }
+    //public void SetBuilding(TMP_Dropdown building)
+    //{ Building = EnumHelper.GetEnum<BuildingName>(building.value); }
+    //public void SetFloor(TMP_Dropdown floor)
+    //{ Floor = EnumHelper.GetEnum<FloorLevel>(floor.value); }
+    //public void SetRoom(TMP_Dropdown room)
+    //{ Room = EnumHelper.GetEnum<RoomNumber>(room.value); }
     public void ResetAttendanceData()
     {
         if (attendanceContainer.childCount > 0)
@@ -140,4 +146,76 @@ public class ViewInformation : MonoBehaviour
     }
 
     #endregion
+
+    public void FilteringBuilding()
+    {
+        buildingEmpty.text = "None";
+        con = new SqlConnection(connectionString);
+        filterBuilding.options.Clear();
+        con.Open();
+        if (con.State == ConnectionState.Open)
+        {
+            query = "SELECT BUILDING_NAME FROM BUILDING_INFO";
+            cmd = new SqlCommand(query, con);
+            rd = cmd.ExecuteReader();
+            filterBuilding.options.Add(new TMP_Dropdown.OptionData() { text = "None" });
+            while (rd.Read())
+            {
+                filterBuilding.options.Add(new TMP_Dropdown.OptionData() { text = rd["BUILDING_NAME"].ToString() });
+            }
+        }
+        //TMP_DropdownItemSelected(filterBuilding, buildingEmpty);
+        //filterRoom.onValueChanged.AddListener(delegate { TMP_DropdownItemSelected(filterBuilding, buildingEmpty); });
+    }
+    public void FilteringFloor()
+    {
+        floorEmpty.text = filterFloor.options[filterFloor.value].text;
+        con = new SqlConnection(connectionString);
+        filterFloor.options.Clear();
+        con.Open();
+        if (con.State == ConnectionState.Open)
+        {
+            query = "SELECT FLOOR_NO FROM FLOOR_INFO WHERE BUILDING_NAME = '"+filterBuilding.options[filterBuilding.value].text+"'";
+            cmd = new SqlCommand(query, con);
+            rd = cmd.ExecuteReader();
+            filterFloor.options.Add(new TMP_Dropdown.OptionData() { text = "None" });
+            while (rd.Read())
+            {
+                filterFloor.options.Add(new TMP_Dropdown.OptionData() { text = rd["FLOOR_NO"].ToString() });
+            }
+        }
+        
+        //TMP_DropdownItemSelected(filterFloor, floorEmpty);
+        //filterRoom.onValueChanged.AddListener(delegate { TMP_DropdownItemSelected(filterFloor, floorEmpty); });
+    }
+    public void FilteringRoom()
+    {
+        roomEmpty.text = filterRoom.options[filterRoom.value].text;
+        con = new SqlConnection(connectionString);
+        filterRoom.options.Clear();
+        con.Open();
+        if (con.State == ConnectionState.Open)
+        {
+            query = "SELECT ROOM_NO FROM ROOM_INFO WHERE BUILDING_NAME ='"+ filterBuilding.options[filterBuilding.value].text + "'AND FLOOR_NO = '"+ filterFloor.options[filterFloor.value].text + "'";
+            cmd = new SqlCommand(query, con);
+            rd = cmd.ExecuteReader();
+            filterRoom.options.Add(new TMP_Dropdown.OptionData() { text = "None"});
+            while (rd.Read())
+            {
+                filterRoom.options.Add(new TMP_Dropdown.OptionData() { text = rd["ROOM_NO"].ToString() });
+            }
+        }
+        //TMP_DropdownItemSelected(filterRoom, roomEmpty);
+        //filterRoom.onValueChanged.AddListener(delegate { TMP_DropdownItemSelected(filterRoom, roomEmpty); });
+    }
+    //public void TMP_DropdownItemSelected(TMP_Dropdown dropdown, TMP_Text name)
+    //{
+    //    if (name.text.Equals("None"))
+    //    {
+    //        int index = 0;
+    //        name.text = dropdown.options[index].text;
+    //    }           
+            
+        
+    //}
 }
